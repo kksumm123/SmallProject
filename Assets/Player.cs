4467908d6cddaf5890d3d5386e1fc2a7a7b8866e
kksumm123 <kksumm123@gmail.com>
2021-09-18 17:20:35 +0900

    기본 move() 완성

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Animator animator;
+    float speed = 5f;
+
     void Start()
     {
-        
+        animator = GetComponentInChildren<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Move();
     }
+
+    #region Move
+    Vector2 move;
+    void Move()
+    {
+        move = Vector2.zero;
+        if (GK(KeyCode.A)) move.x = -1;
+        if (GK(KeyCode.D)) move.x = 1;
+
+        if (move != Vector2.zero)
+        {
+            transform.Translate(speed * Time.deltaTime * move);
+        }
+    }
+    #endregion Move
+
+    #region Methods
+    bool GK(KeyCode _keycode) => Input.GetKey(_keycode);
+    bool GKD(KeyCode _keycode) => Input.GetKeyDown(_keycode);
+    #endregion Methods
 }
