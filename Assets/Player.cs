45cc389f0274d336e7d3f70ba211da9e9a31d2ae
kksumm123 <kksumm123@gmail.com>
2021-09-18 17:27:21 +0900

    StateType구현

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -10,19 +10,50 @@ public class Player : MonoBehaviour
     void Start()
     {
         animator = GetComponentInChildren<Animator>();
+        State = StateType.Idle;
     }
 
     void Update()
     {
+        // 2초간 쉬는 임시코드
+        if (Time.time < 2)
+            return;
+
         Move();
     }
 
     #region Move
     void Move()
     {
+        State = StateType.Running;
         transform.Translate(speed * Time.deltaTime * Vector2.right);
     }
     #endregion Move
+
+    #region State
+    enum StateType
+    {
+        None,
+        Idle,
+        Running,
+        Jump,
+        Fall,
+    }
+    StateType m_state = StateType.None;
+
+    StateType State 
+    {
+        get => m_state;
+        set
+        {
+            if (m_state == value)
+                return;
+
+            print($"PlayerState : {m_state} -> {value}");
+            m_state = value;
+        }
+    }
+    #endregion State
 
     #region Methods
     bool GK(KeyCode _keycode) => Input.GetKey(_keycode);
